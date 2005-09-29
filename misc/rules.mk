286f95fe5915de952a52040a1ac09447d09d7033
Rudá Moura <ruda.moura@gmail.com>
2005-09-29 00:53:50 +0000

    "The great simplification" - FLAVOR is dead - TITLE is no more, use PACKAGE instead - ARCHIVER is ZIP now - retrive method is now in rules.mk - package method becames now pkg and resources methods

--- misc/rules.mk
+++ misc/rules.mk
@@ -5,22 +5,22 @@
 #
 
 # Don't need to modify in most of the cases
-FLAVOR=		panther
 CWD:=		$(shell pwd)
+PACKAGE=	$(NAME)-$(VERSION)
 CONTENTS=	$(CWD)/contents
 RESOURCES=	$(CWD)/resources
 PACKAGEMAKER=	/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker -build
-ARCHIVER=	zip -r -0
-MKDMG=		hdiutil create
+ZIP=		zip -r -0
+CREATEDMG=	hdiutil create
 TOUCH=		@date >
-UNINSTALLER=	$(CONTENTS)/usr/local/sbin/uninstall-$(TITLE).sh
+UNINSTALLER=	$(CONTENTS)/usr/local/sbin/uninstall-$(PACKAGE).sh
 
 # make without action build and install
 all: install
 
 # About this package
 about:
-	@echo "$(TITLE) - $(DESCRIPTION)"
+	@echo "$(PACKAGE) - $(DESCRIPTION)"
 
 # Help me!
 help:
@@ -31,34 +31,48 @@ help:
 	@echo "  prep		explode source, apply patches, etc"
 	@echo "  build		configure software and then build it"
 	@echo "  install	install software into a directory"
-	@echo "  package	create a package (.pkg)"
+	@echo "  pkg	create a package (.pkg)"
 	@echo "  dmg		create a disk image (.dmg)"
-	@echo "  archive	archive (zip) the package for distribution"
-	@echo "  all		do retrive, prep, build, install and package together"
+	@echo "  zip		zip package for distribution (.pkg.zip)"
+	@echo "  all		do retrive, prep, build and install"
 	@echo "  clean"
 	@echo "  distclean"
 
+retrive:
+	curl -O $(URL)/$(SOURCE)
+	$(TOUCH) retrive
+
 # Package's meta-information
 plist:
-	sed "s/@TITLE@/$(TITLE)/g ; s/@NAME@/$(NAME)/g ; s/@VERSION@/$(VERSION)/g" ../../misc/Info.plist.in > Info.plist
+	sed "s/@PACKAGE@/$(PACKAGE)/g ; s/@NAME@/$(NAME)/g ; s/@VERSION@/$(VERSION)/g" ../../misc/Info.plist.in > Info.plist
 	sed "s/@DESCRIPTION@/$(DESCRIPTION)/g ; s/@NAME@/$(NAME)/g ; s/@VERSION@/$(VERSION)/g" ../../misc/Description.plist.in > Description.plist
 	$(TOUCH) plist
 
+resources:
+	mkdir -p $(RESOURCES)
+	cp -f $(PACKAGE)/COPYING $(RESOURCES)/License.txt
+	cp -f $(PACKAGE)/README $(RESOURCES)/ReadMe.txt
+
+pkg: install plist uninstaller resources
+	sudo chown -R root:wheel $(CONTENTS)
+	$(PACKAGEMAKER) -p $(CWD)/$(PACKAGE).pkg -f $(CONTENTS) -r $(RESOURCES) -i Info.plist -d Description.plist
+	$(TOUCH) pkg
+
 # DMG image
-dmg: package
-	$(MKDMG) -srcfolder $(TITLE).pkg ../../packages/$(TITLE)_$(FLAVOR)
+dmg: pkg
+	$(CREATEDMG) -srcfolder $(PACKAGE).pkg ../../packages/$(PACKAGE)
 	touch dmg
 
 # Archive package for distribution
-archive: package
-	$(ARCHIVER) ../../packages/$(TITLE)_$(FLAVOR).pkg.zip $(TITLE).pkg
-	$(TOUCH) archive
+zip: pkg
+	$(ZIP) ../../packages/$(PACKAGE).pkg.zip $(PACKAGE).pkg
+	$(TOUCH) zip
 
 uninstaller: install
 	mkdir -p $(CONTENTS)/usr/local/sbin
 	echo "#!/bin/sh" > $(UNINSTALLER)
-	echo "echo Uninstaller script for $(TITLE) package" >> $(UNINSTALLER)
-	echo "rm -rf  /Library/Receipts/$(TITLE).pkg" >> $(UNINSTALLER)
+	echo "echo Uninstaller script for $(PACKAGE) package" >> $(UNINSTALLER)
+	echo "rm -rf  /Library/Receipts/$(PACKAGE).pkg" >> $(UNINSTALLER)
 	cd $(CONTENTS) ; \
 	find . \! -type d -exec echo "rm -f {}" \; >> $(UNINSTALLER) ; \
 	sed "s/^rm -f \./rm -f /" $(UNINSTALLER) > $(UNINSTALLER).new ; \
@@ -67,21 +81,21 @@ uninstaller: install
 
 # Cleanup
 dmgclean:
-	rm -f dmg ../../packages/$(TITLE)_$(FLAVOR).dmg
+	rm -f dmg ../../packages/$(PACKAGE).dmg
 
-archiveclean:
-	rm -f archive ../../packages/$(TITLE)_$(FLAVOR).pkg.zip
+zipclean:
+	rm -f zip ../../packages/$(PACKAGE).pkg.zip
 
 pkgclean:
-	rm -rf package install plist uninstaller $(TITLE).pkg $(RESOURCES) Info.plist Description.plist
+	rm -rf pkg install plist uninstaller $(PACKAGE).pkg $(RESOURCES) Info.plist Description.plist
 	sudo sudo rm -rf $(CONTENTS)
 
 buildclean:
-	rm -rf build prep $(TITLE)
+	rm -rf build prep $(PACKAGE)
 
 retriveclean:
 	rm -f retrive $(SOURCE)
 
 clean: pkgclean buildclean
 
-distclean: clean archiveclean dmgclean retriveclean
+distclean: clean zipclean dmgclean retriveclean
